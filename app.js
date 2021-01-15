a701f281a8e4951f7de9c94f59ada99b6687cfc7
nazarenko-by <bodean56@ukr.net>
2021-01-15 20:48:34 +0200

    added gumburger menu

--- app.js
+++ app.js
@@ -32,3 +32,17 @@ modal.addEventListener("click", function(e){
     }
 })
 
+//mobile menu
+let mobileMenu = document.querySelector(".nav-mobile-menu");
+let mainMenu = document.querySelector(".menu");
+
+mobileMenu.addEventListener("click", function(){
+    mobileMenu.classList.toggle("active-menu");
+    if(mobileMenu.classList.contains("active-menu")){
+        mainMenu.classList.add("active-menu");
+    } else{
+        mainMenu.classList.remove("active-menu")
+    }
+})
+
+
